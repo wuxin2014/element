5d3d9f64efaf9de95aa3031a9df355348e15b2e3
w-zhangmeiyan <meiyan.zhang1@gientech.com>
2024-06-24 16:23:09 +0800

    cascader-popper

--- examples/play/pages/CascaderDemo1.vue
+++ examples/play/pages/CascaderDemo1.vue
@@ -1,310 +1,138 @@
 <template>
-    <div style="padding: 20px">
-        <el-form :model="searchForm" label-width="100px">
-            <el-row>
-                <el-col :span="8">
-                    <el-form-item label="申请时间">
-                        <el-date-picker
-                            v-model="searchForm.effect_date"
-                            type="daterange"
-                            range-separator="~"
-                            start-placeholder="开始曰期"
-                            end-placeholder="结束日期"
-                            format="yyyyMMdd"
-                            value-format="yyyyMMdd"
-                            :pickerOptions="startPickerOptions"
-                            :append-to-body="true"
-                            :offset="-100"
-                            style="width: 100%">
-                        </el-date-picker>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="过期时间">
-                        <el-date-picker
-                            v-model="searchForm.expire_date"
-                            type="daterange"
-                            range-separator="~"
-                            start-placeholder="开始曰期"
-                            end-placeholder="结束日期"
-                            format="yyyyMMdd"
-                            value-format="yyyyMMdd"
-                            :pickerOptions="endPickerOptions"
-                            :append-to-body="true"
-                            :offset="0"
-                            style="width: 100%">
-                        </el-date-picker>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="级联选项">
-                        <el-cascader
-                            v-model="searchForm.value"
-                            :props="props"
-                            :options="options"
-                            :append-to-body="true"
-                            :offset="-400"
-                            style="width: 100%"
-                            @change="handleChange">
-                        </el-cascader>
-                        <!-- <el-cascader
-                            v-model="searchForm.value"
-                            :props="props"
-                            :options="options"
-                            :append-to-body="false"
-                            popper-class="custom-popper-class"
-                            style="width: 100%"
-                            @change="handleChange">
-                        </el-cascader> -->
-                        <!-- <el-cascader :options="options">
-                            <template slot-scope="{ node, data }">
-                                <span>{{ data.label }}</span>
-                                <span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
-                            </template>
-                        </el-cascader> -->
-                    </el-form-item>
-                </el-col>
-            </el-row>
-        </el-form>
-    </div>
+  <div ref="comp" style="padding: 20px 0">
+    <el-form :model="searchForm" label-width="100px">
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="申请时间">
+            <el-date-picker
+              v-model="searchForm.effect_date"
+              type="daterange"
+              range-separator="~"
+              start-placeholder="开始曰期"
+              end-placeholder="结束日期"
+              format="yyyyMMdd"
+              value-format="yyyyMMdd"
+              :pickerOptions="startPickerOptions"
+              :append-to-body="true"
+              :offset="-100"
+              style="width: 100%">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="过期时间">
+            <el-date-picker
+              v-model="searchForm.expire_date"
+              type="daterange"
+              range-separator="~"
+              start-placeholder="开始曰期"
+              end-placeholder="结束日期"
+              format="yyyyMMdd"
+              value-format="yyyyMMdd"
+              :pickerOptions="endPickerOptions"
+              :append-to-body="true"
+              :offset="0"
+              style="width: 100%">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="级联选项">
+            <!-- placement="bottom-end" :offset="210" align="right" :arrowOffset="400" :append-to-body="false" popper-class="custom-popper-class"-->
+            <el-cascader
+              ref="cascaderRef"
+              v-model="searchForm.value"
+              :props="props"
+              :options="options"
+              style="width: 100%"
+              @change="handleChange">
+            </el-cascader>
+            <!-- <el-cascader :options="options">
+              <template slot-scope="{ node, data }">
+                <span>{{ data.label }}</span>
+                <span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
+              </template>
+            </el-cascader> -->
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            props: {
-                // multiple: true
-                multiple: false
-            },
-            searchForm: {
-                value: [], // [["zhinan", "shejiyuanze", "yizhi"], ["zhinan", "shejiyuanze", "fankui"]],
-                effect_date: [],
-                expire_date: []
-            },
-            startPickerOptions: {
-                disabledDate(time) {
-                    // return that.$moment(time).format('YYYYMMDD') < that.$moment().subtract(1, 'years').format('YYYYMMDD')
-                }
-            },
-            endPickerOptions: {
-                disabledDate(time) {
-                    // return that.$moment(time).format('YYYYMMDD') > that.$moment().add(1, 'years').format('YYYYMMDD')
-                }
-            },
-            options: [{
-                value: 'zhinan',
-                label: '指南',
-                children: [{
-                    value: 'shejiyuanze',
-                    label: '设计原则',
-                    children: [{
-                        value: 'yizhi',
-                        label: '一致'
-                    }, {
-                        value: 'fankui',
-                        label: '反馈'
-                    }, {
-                        value: 'xiaolv',
-                        label: '效率'
-                    }, {
-                        value: 'kekong',
-                        label: '可控'
-                    }]
-                }, {
-                    value: 'daohang',
-                    label: '导航',
-                    children: [{
-                        value: 'cexiangdaohang',
-                        label: '侧向导航'
-                    }, {
-                        value: 'dingbudaohang',
-                        label: '顶部导航'
-                    }]
-                }]
-            }, {
-                value: 'zujian',
-                label: '组件',
-                children: [{
-                    value: 'basic',
-                    label: 'Basic',
-                    children: [{
-                        value: 'layout',
-                        label: 'Layout 布局'
-                    }, {
-                        value: 'color',
-                        label: 'Color 色彩'
-                    }, {
-                        value: 'typography',
-                        label: 'Typography 字体'
-                    }, {
-                        value: 'icon',
-                        label: 'Icon 图标'
-                    }, {
-                        value: 'button',
-                        label: 'Button 按钮'
-                    }]
-                }, {
-                    value: 'form',
-                    label: 'Form',
-                    children: [{
-                        value: 'radio',
-                        label: 'Radio 单选框'
-                    }, {
-                        value: 'checkbox',
-                        label: 'Checkbox 多选框'
-                    }, {
-                        value: 'input',
-                        label: 'Input 输入框'
-                    }, {
-                        value: 'input-number',
-                        label: 'InputNumber 计数器'
-                    }, {
-                        value: 'select',
-                        label: 'Select 选择器'
-                    }, {
-                        value: 'cascader',
-                        label: 'Cascader 级联选择器'
-                    }, {
-                        value: 'switch',
-                        label: 'Switch 开关'
-                    }, {
-                        value: 'slider',
-                        label: 'Slider 滑块'
-                    }, {
-                        value: 'time-picker',
-                        label: 'TimePicker 时间选择器'
-                    }, {
-                        value: 'date-picker',
-                        label: 'DatePicker 日期选择器'
-                    }, {
-                        value: 'datetime-picker',
-                        label: 'DateTimePicker 日期时间选择器'
-                    }, {
-                        value: 'upload',
-                        label: 'Upload 上传'
-                    }, {
-                        value: 'rate',
-                        label: 'Rate 评分'
-                    }, {
-                        value: 'form',
-                        label: 'Form 表单'
-                    }]
-                }, {
-                    value: 'data',
-                    label: 'Data',
-                    children: [{
-                        value: 'table',
-                        label: 'Table 表格'
-                    }, {
-                        value: 'tag',
-                        label: 'Tag 标签'
-                    }, {
-                        value: 'progress',
-                        label: 'Progress 进度条'
-                    }, {
-                        value: 'tree',
-                        label: 'Tree 树形控件'
-                    }, {
-                        value: 'pagination',
-                        label: 'Pagination 分页'
-                    }, {
-                        value: 'badge',
-                        label: 'Badge 标记'
-                    }]
-                }, {
-                    value: 'notice',
-                    label: 'Notice',
-                    children: [{
-                        value: 'alert',
-                        label: 'Alert 警告'
-                    }, {
-                        value: 'loading',
-                        label: 'Loading 加载'
-                    }, {
-                        value: 'message',
-                        label: 'Message 消息提示'
-                    }, {
-                        value: 'message-box',
-                        label: 'MessageBox 弹框'
-                    }, {
-                        value: 'notification',
-                        label: 'Notification 通知'
-                    }]
-                }, {
-                    value: 'navigation',
-                    label: 'Navigation',
-                    children: [{
-                        value: 'menu',
-                        label: 'NavMenu 导航菜单'
-                    }, {
-                        value: 'tabs',
-                        label: 'Tabs 标签页'
-                    }, {
-                        value: 'breadcrumb',
-                        label: 'Breadcrumb 面包屑'
-                    }, {
-                        value: 'dropdown',
-                        label: 'Dropdown 下拉菜单'
-                    }, {
-                        value: 'steps',
-                        label: 'Steps 步骤条'
-                    }]
-                }, {
-                    value: 'others',
-                    label: 'Others',
-                    children: [{
-                        value: 'dialog',
-                        label: 'Dialog 对话框'
-                    }, {
-                        value: 'tooltip',
-                        label: 'Tooltip 文字提示'
-                    }, {
-                        value: 'popover',
-                        label: 'Popover 弹出框'
-                    }, {
-                        value: 'card',
-                        label: 'Card 卡片'
-                    }, {
-                        value: 'carousel',
-                        label: 'Carousel 走马灯'
-                    }, {
-                        value: 'collapse',
-                        label: 'Collapse 折叠面板'
-                    }]
-                }]
-            }, {
-                value: 'ziyuan',
-                label: '资源',
-                children: [{
-                    value: 'axure',
-                    label: 'Axure Components'
-                }, {
-                    value: 'sketch',
-                    label: 'Sketch Templates'
-                }, {
-                    value: 'jiaohu',
-                    label: '组件交互文档'
-                }]
-            }]
+  data() {
+    return {
+      props: {
+        // multiple: true
+        multiple: false
+      },
+      searchForm: {
+        value: [],
+        effect_date: [],
+        expire_date: []
+      },
+      startPickerOptions: {
+        disabledDate(time) {
+          // return that.$moment(time).format('YYYYMMDD') < that.$moment().subtract(1, 'years').format('YYYYMMDD')
         }
-    },
-    methods: {
-        handleChange(val) {
-            console.log('val===', val)
+      },
+      endPickerOptions: {
+        disabledDate(time) {
+          // return that.$moment(time).format('YYYYMMDD') > that.$moment().add(1, 'years').format('YYYYMMDD')
         }
+      },
+      options: [
+        {
+          value: 'zhinan',
+          label: '指南',
+          children: [
+            {
+              value: 'shejiyuanze',
+              label: '设计原则',
+              children: [
+                {
+                  value: 'yizhi',
+                  label: '一致'
+                }, 
+                {
+                  value: 'fankui',
+                  label: '反馈'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      popperOptions: { gpuAcceleration: true, removeOnDestroy: true },
     }
+  },
+  mounted() {
+    this.popperOptions = { ...this.popperOptions, boundariesElement: this.$refs.comp };
+    // this.$nextTick(()=>{
+    //   this.$refs.cascaderRef.updatePopper()
+    // })
+  },
+  methods: {
+    handleChange(val) {
+      console.log('val===', val)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 body {
-    position: relative;
-    margin: 0;
-    padding: 0;
+  position: relative;
+  margin: 0;
+  padding: 0;
 }
 .custom_poper_class {
   position: absolute !important;
   left: 0 !important;
+}
+.el-cascader-menu__hover-zone {
+  display: none; // 级联hover选择卡顿问题
 }
 </style>
--- examples/play/pages/TooltipDemo.vue
+++ examples/play/pages/TooltipDemo.vue
@@ -1,19 +1,70 @@
 <template>
-    <div>
-        <el-tooltip class="item" effect="dark" content="Top Left 提示文字" placement="top-start" :offset="20">
-            <el-button>上左</el-button>
-        </el-tooltip>
+  <div>
+    <div ref="comp" class="comp">
+      <h3>Hello Vue 2 + Vite</h3>
+      <el-button type="primary">按钮</el-button>
+      <el-tooltip
+        ref="elTooltip"
+        class="item"
+        effect="light"
+        placement="top"
+        :popper-options="popperOptions">
+          <el-button>上边</el-button>
+          <div slot="content" class="tooltipContainer">
+            <div v-for="(item, index) in 100" :key="index">{{ item }}-这是内容</div>
+          </div>
+      </el-tooltip>
     </div>
+    <el-tooltip class="item" effect="dark" content="Top Left 提示文字" placement="top-start" :offset="20">
+      <el-button>上左</el-button>
+    </el-tooltip>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      popperOptions: { boundariesElement: this.$refs.comp, gpuAcceleration: true },
+    }
+  },
+  created() {
+    this.$message('这是一条消息提示')
+  },
+  mounted() {
+    console.log('this.$refs.elTooltip', this.$refs.elTooltip)
+    this.popperOptions = {
+      ...this.popperOptions,
+      boundariesElement: this.$refs.comp,
+    }
+
+    // this.$nextTick(()=>{
+    //   this.$refs.elTooltip.updatePopper()
+    // })
+  },
+}
+</script>
 
 <style>
 body {
-    padding: 0;
-    margin: 0;
-    position: relative;
+  padding: 0;
+  margin: 0;
+  position: relative;
 }
 
 #app {
-    padding: 10px;
+  padding: 10px;
+}
+.tooltipContainer {
+  max-height: 200px;
+  overflow: auto;
+}
+.comp {
+  width: 300px;
+  height: 200px;
+  background: lightblue;
+  margin-top: 300px;
 }
 </style>
+
