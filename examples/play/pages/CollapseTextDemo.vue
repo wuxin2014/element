d825ec07b941ddbfef62f1fceb9080098dc5b8f8
w-zhangmeiyan <meiyan.zhang1@gientech.com>
2024-08-01 09:55:53 +0800

    文本展开跟收起

---
+++ examples/play/pages/CollapseTextDemo.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="collapse-text-container">
+      <CollapseText>
+        闻笛声独惆怅,云深夜未央,是与非都过往,醒来了怎能当梦一场,红尘中毁誉得失如何去量,潇潇血热刀锋凉.
+        闻笛声独惆怅,云深夜未央,是与非都过往,醒来了怎能当梦一场,红尘中毁誉得失如何去量,潇潇血热刀锋凉.
+        闻笛声独惆怅,云深夜未央,是与非都过往,醒来了怎能当梦一场,红尘中毁誉得失如何去量,潇潇血热刀锋凉.
+        闻笛声独惆怅,云深夜未央,是与非都过往,醒来了怎能当梦一场,红尘中毁誉得失如何去量,潇潇血热刀锋凉.
+        闻笛声独惆怅,云深夜未央,是与非都过往,醒来了怎能当梦一场,红尘中毁誉得失如何去量,潇潇血热刀锋凉.
+        闻笛声独惆怅,云深夜未央,是与非都过往,醒来了怎能当梦一场,红尘中毁誉得失如何去量,潇潇血热刀锋凉.
+      </CollapseText>
+    </div>
+    <div class="list-container">
+      <div ref="listRef" class="list" style="position: absolute; visibility: hidden;">
+        <div v-for="(item, index) in list" :key="index" class="item" ref="itemRef">{{ item }}</div>
+      </div>
+      <div class="list">
+        <div v-for="(item, index) in newList" :key="index" class="item">{{ item }}</div>
+        <div v-if="newList.length > 0" class="expand-wrap" @click="handleExpand">展开</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CollapseText from './components/CollapseText.vue'
+
+export default {
+  components: {
+    CollapseText
+  },
+  data() {
+    return {
+      list: ['闻笛声独惆怅', '笛声', '醒来了怎能当梦一场', '是与非都过往', '是非', '梦一场', '潇潇血热刀锋凉'],
+      newList: [],
+      isExpand: false,
+      curIndex: -1
+    }
+  },
+  mounted() {
+    this.layout()
+  },
+  methods: {
+    layout() {
+      this.$nextTick(() => {
+        const clientWidth = this.$refs.listRef.clientWidth;
+        const offsetHeight = this.$refs.listRef.offsetHeight;
+        // const offsetWidth = this.$refs.listRef.scrollWidth;
+        if (offsetHeight > 42) {
+          let widthSum = 0
+          this.$refs.itemRef.forEach((element, index) => {
+            widthSum += element.offsetWidth
+            if (widthSum <= clientWidth - 60) {
+              this.curIndex = index - 1
+            }
+          });
+          this.newList = this.list.slice(0, this.curIndex + 1)
+        }
+      })
+    },
+    handleExpand() {
+      if (this.isExpand) {
+        this.newList = this.list.slice(0, this.curIndex + 1)
+      } else {
+        this.newList = this.list
+      }
+      this.isExpand = !this.isExpand
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.collapse-text-container {
+  width: 600px;
+  padding: 10px;
+  background-color: aliceblue;
+  border-radius: 8px;
+  // box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
+}
+
+.list-container {
+  width: 400px;
+  background-color: aliceblue;
+  padding: 10px;
+  height: 100%;
+  margin-top: 30px;
+
+  .list {
+    width: 400px;
+    height: 100%;
+    overflow: hidden;
+    clear: both;
+  }
+
+  .item {
+    height: 30px;
+    padding: 0 10px;
+    line-height: 30px;
+    border: 1px solid pink;
+    margin: 5px;
+    border-radius: 30px;
+    float: left;
+  }
+
+  .expand-wrap {
+    float: right;
+    width: 50px;
+    height: 30px;
+    margin: 5px;
+    text-align: center;
+    line-height: 30px;
+    background: #3f51b5;
+    border-radius: 4px;
+    color: #fff;
+    cursor: pointer;
+  }
+}
+</style>
--- examples/play/pages/components/CollapseText.vue
+++ examples/play/pages/components/CollapseText.vue
@@ -1,29 +1,14 @@
 <template>
-  <div>
-    <!-- <div class="wrapper">
-      <input :id="id" class="exp" type="checkbox" />
-      <div class="text" ref="textRef">
-        <label v-if="isTextOverflow" class="btn" :for="id"></label>
-        <slot></slot>
-      </div>
-    </div> -->
-    <div class="wrapper">
-      <div ref="textRef" class="text" :class="{expand: isExpand}">
-        <label v-show="isTextOverflow" class="expand-btn" @click="isExpand = !isExpand">{{ isExpand ? '收起' : '展开' }}</label>
-        <slot></slot>
-      </div>
+  <div class="wrapper">
+    <div ref="textRef" class="text" :class="{expand: isExpand}">
+      <label v-show="isTextOverflow" class="expand-btn" @click="isExpand = !isExpand">{{ isExpand ? '收起' : '展开' }}</label>
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    id: {
-      type: String,
-      required: true,
-    },
-  },
   data() {
     return {
       isTextOverflow: false,
@@ -42,12 +27,9 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
   display: flex;
-  width: 800px;
-  padding: 15px;
-  margin: 50px auto;
-  border-radius: 8px;
-  box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
+  width: 100%;
   overflow: hidden;
+
 }
 .text {
   font-size: 20px;
@@ -58,15 +40,14 @@ export default {
   -webkit-box-orient: vertical;
   text-align: justify;
   position: relative;
+  &.expand {
+    -webkit-line-clamp: 999;
+  }
 
   &::before {
     content: "";
     height: calc(100% - 24px);
     float: right;
-  }
-
-  &.expand {
-    -webkit-line-clamp: 999;
   }
 
   .expand-btn {
@@ -81,31 +62,5 @@ export default {
     color: #fff;
     cursor: pointer;
   }
-
-  .btn {
-    float: right;
-    clear: both;
-    margin-left: 10px;
-    font-size: 16px;
-    padding: 0 8px;
-    background: #3f51b5;
-    line-height: 24px;
-    border-radius: 4px;
-    color: #fff;
-    cursor: pointer;
-
-    &::before {
-      content: "展开";
-    }
-  }
-}
-.exp {
-  display: none;
-}
-.exp:checked + .text {
-  -webkit-line-clamp: 999;
-}
-.exp:checked + .text .btn::before {
-  content: "收起";
 }
 </style>
